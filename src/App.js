9debcafe03d7b333d346c374a12535c907504a51
skllup7 <100334469+skllup7@users.noreply.github.com>
2023-07-31 15:13:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ShippingCalculator from './components/ShippingCalculator';
+import './styles.css';
+
+// App Code 
+function App() {
+  return (
+    <div className="App">
+      <ShippingCalculator />
+    </div>
+  );
+}
+
+export default App;
