8b906b690224e354c22b5e4516fbe59ea7aa1d65
skllup7 <100334469+skllup7@users.noreply.github.com>
2023-09-12 11:32:19 +0530

    Update ShippingCalculator.js

--- src/components/ShippingCalculator.js
+++ src/components/ShippingCalculator.js
@@ -1,71 +1,72 @@
-import React, { useState } from 'react'; 
-import Chart from './Chart'; 
-import LoginForm from './LoginForm'; 
-import '../styles.css'; // Import the styles.css file 
-import Select from 'react-select'; // Import the react-select component 
-import IndiaCities from '../data/India-city-names.json'; // Import the India city data 
-import USCities from '../data/US_States_and_Cities.json'; // Import the US city data 
+import React, { useState } from 'react';
+import Chart from './Chart';
+import LoginForm from './LoginForm';
+import '../styles.css'; // Import the styles.css file
+import Select from 'react-select'; // Import the react-select component
+import IndiaCities from '../data/India-city-names.json'; // Import the India city data
+import USCities from '../data/US_States_and_Cities.json'; // Import the US city data
 
- 
-const ShippingCalculator = () => { 
-// Step 3: Declare state variables
-  
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-const [freightRates, setFreightRates] = useState([]);
-const [packageSize, setPackageSize] = useState('');
-const [destination, setDestination] = useState(null);
-const [rate, setRate] = useState('');
-const [packageWeightUnit, setPackageWeightUnit] = useState('kg');
-  
+const ShippingCalculator = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [freightRates, setFreightRates] = useState([]);
+  const [packageSize, setPackageSize] = useState('');
+  const [destination, setDestination] = useState(null);
+  const [rate, setRate] = useState('');
+  const [packageWeightUnit, setPackageWeightUnit] = useState('kg');
 
-// Step 4: Implement the handleLogin function 
-const handleLogin = () => {
-    //setIsLoggedIn();
+  const handleLogin = () => {
+    setIsLoggedIn(true);
   };
- 
-// Step 5: Implement the addFreightRate function 
-const addFreightRate = () => {
-    // if (!packageSize || <add destination> || <add rate> ) {
-    //  alert('Please fill in all the fields.');
-    //   return;
+
+  const addFreightRate = () => {
+    if (!packageSize || !destination || !rate) {
+      alert('Please fill in all the fields.');
+      return;
     }
- 
-  const currency = packageWeightUnit === 'kg' ? 'inr' : 'usd';
 
-  const newRate = { packageSize, destination: destination.label, rate: parseFloat(rate), currency, packageWeightUnit};
-  setFreightRates([...freightRates, newRate]);
+    const currency = packageWeightUnit === 'kg' ? 'inr' : 'usd';
 
-  setPackageSize('');
-  setDestination(null); // Reset the destination to null after adding the rate
-  setRate('');
+    const newRate = {
+      packageSize,
+      destination: destination.label,
+      rate: parseFloat(rate),
+      currency,
+      packageWeightUnit,
+    };
+
+    setFreightRates([...freightRates, newRate]);
+
+    setPackageSize('');
+    setDestination(null); // Reset the destination to null after adding the rate
+    setRate('');
   };
- // Step 6: Implement the getCityOptions function
-const getCityOptions = () => {
+
+  // Get the appropriate cityOptions based on the selected package weight unit
+  const getCityOptions = () => {
+    // Add your logic here based on the selected packageWeightUnit
     if (packageWeightUnit === 'kg') {
-      return Object.keys(IndiaCities).flatMap((country) =>
-         //<Add the code here>
-        }))
-      );
+      // Hint: For 'kg' unit, retrieve city options from IndiaCities data
+      return [];
     } else {
-      return Object.keys(USCities).flatMap((state) =>
-         //<Add the code here>
-        }))
-      );
+      // Hint: For other units, retrieve city options from USCities data
+      return [];
     }
   };
-  
 
-  return ( 
-
-    <div> 
-    {!isLoggedIn ? (
+  return (
+    <div>
+      {/* Check if the user is logged in  */}
+      {!isLoggedIn ? (
+        // Render the login form if the user is not logged in
         <LoginForm onLogin={handleLogin} />
       ) : (
+        // Render the admin dashboard if the user is logged in
         <div className="shipping-calculator-container">
           <h1>Admin Dashboard</h1>
           <div>
             <h2>Add Freight Rate</h2>
             <form>
+              {/* Package Weight Input */}
               <div className="package-weight-container">
                 <input
                   type="text"
@@ -74,24 +75,17 @@ const getCityOptions = () => {
                   onChange={(e) => setPackageSize(e.target.value)}
                   className="package-size-input"
                 />
-                <select
-                  className="select"
-                  value={packageWeightUnit}
-                  onChange={(e) => setPackageWeightUnit(e.target.value)}
-                >
-                  <option value="kg">KG</option>
-                  <option value="lb">LB</option>
-                </select>
+                {/* Select Package Weight Unit (kg or lb) */}
+                {/* Insert select element here with appropriate props */}
               </div>
+
+              {/* City and State Dropdown */}
               <div className="city-state-dropdown">
-                <Select
-                  options={getCityOptions()} // Use the filtered cityOptions based on package weight unit
-                  value={destination}
-                  onChange={(selectedOption) => setDestination(selectedOption)}
-                  placeholder="Select City and State"
-                />
+                {/* Insert Select component here with appropriate props */}
               </div>
               <br />
+
+              {/* Shipping Rate Input */}
               <div className="shipping-rate-input">
                 <input
                   type="text"
@@ -99,32 +93,21 @@ const getCityOptions = () => {
                   value={rate}
                   onChange={(e) => setRate(e.target.value)}
                 />
-                <select
-                  className="select2"
-                  value={packageWeightUnit === 'kg' ? 'inr' : 'usd'}
-                  onChange={(e) => setPackageWeightUnit(e.target.value === 'inr' ? 'kg' : 'lb')}
-                >
-                  <option value="inr" className="inr-option">
-                    INR
-                  </option>
-                  <option value="usd" className="usd-option">
-                    USD
-                  </option>
-                </select>
+                {/* Select Currency (INR or USD) */}
+                {/* Insert select element here with appropriate props */}
               </div>
-              <button type="button" onClick={addFreightRate}>
-                Add Rate
-              </button>
+
+              {/* Add Rate Button */}
+              {/* Insert button element here with appropriate props */}
             </form>
           </div>
-          <Chart freightRates={freightRates} packageWeightUnit={packageWeightUnit} />
+
+          {/* Display a chart showing freight rates */}
+          {/* Insert Chart component here with appropriate props */}
         </div>
       )}
     </div>
   );
 };
- 
 
-export default ShippingCalculator; 
-
- 
+export default ShippingCalculator;
